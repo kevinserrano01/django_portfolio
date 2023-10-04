057c47819bb3e920b9da1d29dc79be65b2e4a270
Kev <kevinserrano010@gmail.com>
2023-10-04 14:02:32 -0300

    Nueva migracion con la app Blog

--- blog/models.py
+++ blog/models.py
@@ -1,2 +1,9 @@
 from django.db import models
+from django.db.models.fields import CharField
+import datetime
 
+class Post(models.Model):
+    title = CharField(max_length=100)
+    description = models.TextField()
+    image = models.ImageField(upload_to='blog/images') # Crea nueva carpeta para las imagenes
+    date = models.DateField(datetime.date.today) # Fecha actual
--- django_portfolio/urls.py
+++ django_portfolio/urls.py
@@ -1,12 +1,14 @@
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 from django.conf.urls.static import static
 from django.conf import settings
 from portfolio import views
+from blog import urls
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', views.home, name='home')
+    path('', views.home, name='home'),
+    path('blog/', include('blog.urls'), name='posts'),
 ]
 
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
