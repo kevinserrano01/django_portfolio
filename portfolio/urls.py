785c5b942a695e682c1d2ba7f73cc39d4f8b9027
Kev <kevinserrano010@gmail.com>
2023-10-07 15:56:46 -0300

    Actulice el urls de mi app portfolio y esta listo para el deploy

--- portfolio/urls.py
+++ portfolio/urls.py
@@ -8,4 +8,4 @@ urlpatterns = [
     path('projects/', get_projects, name='projects'),
 ]
 
-urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
