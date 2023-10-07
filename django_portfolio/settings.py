6d4672366084bc72b562b3a12bcb935ab770176d
Kev <kevinserrano010@gmail.com>
2023-10-07 14:07:17 -0300

    Aplicacion para hosting pythonanywhere.com

--- django_portfolio/settings.py
+++ django_portfolio/settings.py
@@ -11,6 +11,8 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 
 from pathlib import Path
+import os
+# import dj_database_url
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -25,7 +27,7 @@ SECRET_KEY = 'django-insecure-8&oxs3v8a&&boooh^ab&m+1)63477y9_bk37y&yb*$du2+cfii
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['kevdeveloper.pythonanywhere.com']
 
 
 # Application definition
